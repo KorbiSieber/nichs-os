ab6fd14352616eadba8beae84a52817f91c649de
Korbinian Sieber <ksieber@hm.edu>
2026-03-11 20:08:07 +0100

    Add omz configuration

--- home.nix
+++ home.nix
@@ -1,14 +1,26 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "korbi";
-  home.homeDirectory = "/home/korbi";
-  home.stateVersion = "25.05";
+    home.username = "korbi";
+    home.homeDirectory = "/home/korbi";
+    home.stateVersion = "25.05";
 
-  programs.home-manager.enable = true;
+    programs.home-manager.enable = true;
 
-  programs.zsh = {
-    enable = true;
-    # your zsh config here
-  };
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+
+        oh-my-zsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = [
+            "git"
+            "sudo"
+            "docker"
+        ];
+        };
+    };
 }
